9ed4517103f02915a9f2b25e78f3e74060baca25
jyoti-wp <sim_s006@icloud.com>
2023-06-06 13:09:17 +0530

    first console.log commit

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,6 +4,10 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for (let i = 0; i < 10; i = i + 1) {
+    console.log(i);
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
 
 // Using `console.log` log all the od values from 1 to 10.
